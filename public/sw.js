e2898240d3e88988e27c1aceaef9b879ab1ad2f0
Jausaf <jausafnishatakbar@gmail.com>
2026-07-01 16:17:31 +0530

    Add service worker with caching functionality

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,14 @@
 
+const CACHE_NAME = "scc-test-app-v1";
+
+self.addEventListener("install", function (event) {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(fetch(event.request));
+});
